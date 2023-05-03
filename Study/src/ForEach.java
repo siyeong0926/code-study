9c1c269b76d568d0cb04e6f35f054a999af7d3fd
siyeong0926 <siyeong0926@naver.com>
2023-05-03 23:00:25 +0900

    0503 forEach  , phone

---
+++ Study/src/ForEach.java
@@ -0,0 +1,11 @@
+public class ForEach {
+    public static void main(String[] args){
+
+        String[] numbers = {"one", "two", "three"};
+        for(String number: numbers) {
+            System.out.println(number);
+        }
+
+    }
+
+}
