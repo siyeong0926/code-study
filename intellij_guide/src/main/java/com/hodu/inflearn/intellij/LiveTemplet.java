7c2a7e44fc789a47c539aa9c73008c343a7c9a1e
siyeong0926 <siyeong0926@naver.com>
2023-06-23 22:25:34 +0900

    0623 JavaStudy

---
+++ intellij_guide/src/main/java/com/hodu/inflearn/intellij/LiveTemplet.java
@@ -0,0 +1,15 @@
+package com.hodu.inflearn.intellij;
+
+public class LiveTemplet {
+
+    public static void main(String[] args) {
+        System.out.println();
+        String a = "a";
+
+
+    }
+
+
+}
+
+
