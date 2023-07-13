4aa2c95eaa9825362a0c075b1ee287ac4b9b861d
siyeong0926 <siyeong0926@naver.com>
2023-07-13 22:30:47 +0900

    0713 JavaStudy

---
+++ Study/src/Study03/06_Class_Inherit/src/ex03_abstract/MainClass.java
@@ -0,0 +1,18 @@
+package ex03_abstract;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		/*
+		//추상클래스는 new하지 못한다.
+		Person p1 = new Person();
+		p1.eat();
+		*/
+		
+		Student s1 = new Student();
+		s1.sleep();
+		s1.study();
+		
+	}
+
+}
